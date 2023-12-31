92f3f384e5546ac8f393d010c3b1ee86fd11898c
Dan Timofte <dantimofte@proton.me>
2023-12-31 16:15:29 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = "1.0.0"
+# Runtime dependencies. See requirements.txt for development dependencies.
+DEPENDENCIES = [
+    "aiohttp==3.9.1",
+]
+
+setup(
+    name="ewelink",
+    version=VERSION,
+    description="Ewelink API",
+    url="https://github.com/AceExpert/ewelink-api-python",
+    license="",
+    packages=find_packages(),
+    install_requires=DEPENDENCIES,
+    include_package_data=True,
+    keywords=[],
+    classifiers=[],
+    zip_safe=True,
+    entry_points={},
+)
